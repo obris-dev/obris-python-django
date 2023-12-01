c2cd164d9ff7ce4e80767d7fc5c247aba4d2aead
Josh Panka <josh.panka@gmail.com>
2023-11-30 20:05:28 -0500

    allowed host env variable

--- django_example/django_example/settings.py
+++ django_example/django_example/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-vv55oeh3uvt2xhudg!9nv8-8n2#1x$3j27jimf^so0e+5l7(&y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOST = os.getenv("ALLOWED_HOST")
+
+ALLOWED_HOSTS = [
+    ALLOWED_HOST,
+]
 
 
 STATIC_URL = "static/"
--- django_example/django_example/urls.py
+++ django_example/django_example/urls.py
@@ -14,14 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.views.generic import RedirectView
-
 from django.urls import path
 
 from .views import ExampleTemplateView
 
 
 urlpatterns = [
-    path('favicon.ico', RedirectView.as_view(url='/static/images/favicon.ico')),
     path('', ExampleTemplateView.as_view()),
 ]
